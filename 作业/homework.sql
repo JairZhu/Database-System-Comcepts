e079b751d1d3409f727bf995715294c4ac77d9f9
XIAOXIN\73974 <739741104@qq.com>
2018-11-06 14:58:55 +0800

    no message

--- 作业/homework.sql
+++
@@ -1,6 +0,0 @@
-SELECT M.name
-FROM member M
-WHERE 5 < (SELECT count(isbn)
-           FROM book NATURAL JOIN borrowed AS T
-           WHERE M.memb_no = T.memb_no
-           GROUP BY T.publisher);
